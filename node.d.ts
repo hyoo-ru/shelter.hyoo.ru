5f2ba5e3319257eb107d4042fb7a5de246937aef
nin-jin <nin-jin@users.noreply.github.com>
2020-12-06 07:19:39 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -2386,7 +2386,6 @@ declare namespace $ {
             target: string;
             download: string;
             mol_link_current: boolean;
-            mol_theme: any;
         };
         sub(): readonly (string | number | boolean | $mol_view | Node)[];
         arg(): {};
@@ -2398,7 +2397,6 @@ declare namespace $ {
         target(): string;
         file_name(): string;
         current(): boolean;
-        theme(): any;
         event_click(event?: any): any;
         click(event?: any): any;
     }
@@ -2437,7 +2435,6 @@ declare namespace $.$$ {
         event_click(event?: Event): void;
         file_name(): string;
         minimal_height(): number;
-        theme(): "$mol_theme_base" | null;
     }
 }
 
@@ -2492,7 +2489,6 @@ declare namespace $ {
             title: string;
             target: string;
             mol_link_current: boolean;
-            mol_theme: any;
         };
         url_load(val?: any): any;
         style_bg(): string;
@@ -2592,7 +2588,7 @@ declare namespace $.$$ {
 
 declare namespace $ {
     class $mol_switch extends $mol_view {
-        Option(id: any): $$.$mol_switch_option;
+        Option(id: any): $$.$mol_check;
         value(val?: any): any;
         options(): {};
         keys(): readonly string[];
@@ -2603,19 +2599,6 @@ declare namespace $ {
         enabled(): boolean;
         option_enabled(id: any): boolean;
         items(): readonly $mol_check[];
-    }
-    class $mol_switch_option extends $mol_check {
-        minimal_height(): number;
-        attr(): {
-            mol_theme: string;
-            mol_check_checked: any;
-            "aria-checked": any;
-            role: string;
-            disabled: boolean;
-            tabindex: number;
-            title: string;
-        };
-        theme(): string;
     }
 }
 
@@ -2629,12 +2612,9 @@ declare namespace $.$$ {
             [key: string]: string;
         };
         keys(): string[];
-        items(): $mol_switch_option[];
+        items(): $mol_check[];
         option_title(key: string): string;
         option_checked(key: string, next?: boolean): boolean | undefined;
-    }
-    class $mol_switch_option extends $.$mol_switch_option {
-        theme(): "" | "$mol_theme_base";
     }
 }
 
@@ -2956,7 +2936,6 @@ declare namespace $ {
             target: string;
             download: string;
             mol_link_current: boolean;
-            mol_theme: any;
         };
         uri(): any;
         content(val?: any): any;
@@ -3530,7 +3509,6 @@ declare namespace $ {
             target: string;
             download: string;
             mol_link_current: boolean;
-            mol_theme: any;
         };
         sub(): readonly any[];
         id(): string;
--- web.d.ts
+++ web.d.ts
@@ -2362,7 +2362,6 @@ declare namespace $ {
             target: string;
             download: string;
             mol_link_current: boolean;
-            mol_theme: any;
         };
         sub(): readonly (string | number | boolean | Node | $mol_view)[];
         arg(): {};
@@ -2374,7 +2373,6 @@ declare namespace $ {
         target(): string;
         file_name(): string;
         current(): boolean;
-        theme(): any;
         event_click(event?: any): any;
         click(event?: any): any;
     }
@@ -2419,7 +2417,6 @@ declare namespace $.$$ {
         event_click(event?: Event): void;
         file_name(): string;
         minimal_height(): number;
-        theme(): "$mol_theme_base" | null;
     }
 }
 
@@ -2474,7 +2471,6 @@ declare namespace $ {
             title: string;
             target: string;
             mol_link_current: boolean;
-            mol_theme: any;
         };
         url_load(val?: any): any;
         style_bg(): string;
@@ -2574,7 +2570,7 @@ declare namespace $.$$ {
 
 declare namespace $ {
     class $mol_switch extends $mol_view {
-        Option(id: any): $$.$mol_switch_option;
+        Option(id: any): $$.$mol_check;
         value(val?: any): any;
         options(): {};
         keys(): readonly string[];
@@ -2585,19 +2581,6 @@ declare namespace $ {
         enabled(): boolean;
         option_enabled(id: any): boolean;
         items(): readonly $mol_check[];
-    }
-    class $mol_switch_option extends $mol_check {
-        minimal_height(): number;
-        attr(): {
-            mol_theme: string;
-            mol_check_checked: any;
-            "aria-checked": any;
-            role: string;
-            disabled: boolean;
-            tabindex: number;
-            title: string;
-        };
-        theme(): string;
     }
 }
 
@@ -2611,12 +2594,9 @@ declare namespace $.$$ {
             [key: string]: string;
         };
         keys(): string[];
-        items(): $mol_switch_option[];
+        items(): $mol_check[];
         option_title(key: string): string;
         option_checked(key: string, next?: boolean): boolean | undefined;
-    }
-    class $mol_switch_option extends $.$mol_switch_option {
-        theme(): "" | "$mol_theme_base";
     }
 }
 
@@ -2938,7 +2918,6 @@ declare namespace $ {
             target: string;
             download: string;
             mol_link_current: boolean;
-            mol_theme: any;
         };
         uri(): any;
         content(val?: any): any;
@@ -3512,7 +3491,6 @@ declare namespace $ {
             target: string;
             download: string;
             mol_link_current: boolean;
-            mol_theme: any;
         };
         sub(): readonly any[];
         id(): string;
