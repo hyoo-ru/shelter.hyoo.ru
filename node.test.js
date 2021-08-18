6841c0aa14b3aa3971abd1fd051161d8c1acc693
nin-jin <nin-jin@users.noreply.github.com>
2021-08-18 07:06:18 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -12939,7 +12939,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
-    class $mol_grid extends $.$mol_scroll {
+    class $mol_grid extends $.$mol_view {
         row_height() {
             return 32;
         }
