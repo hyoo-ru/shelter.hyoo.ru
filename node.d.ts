43e4151d1fdd60c846193041c80bde4b99c452f8
nin-jin <nin-jin@users.noreply.github.com>
2023-03-21 07:17:23 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -3039,6 +3039,7 @@ declare namespace $ {
         Row(id: any): $$.$mol_text_code_row;
         rows(): readonly any[];
         Rows(): $$.$mol_list;
+        text_export(): string;
         Copy(): $$.$mol_button_copy;
     }
 }
@@ -3075,6 +3076,7 @@ declare namespace $.$$ {
         }>;
         uri_base(): string;
         uri_resolve(uri: string): string;
+        text_export(): string;
     }
 }
 
