1a7759836463c6b274622a251fd5dd6d2c197495
nin-jin <nin-jin@users.noreply.github.com>
2026-03-17 07:52:01 +0000

    Deploying to gh-pages from @ hyoo-ru/shelter.hyoo.ru@7c85c99b5b0fffe68d9507aa0197cbe7dd95d735 🚀

--- node.mjs
+++ node.mjs
@@ -10415,6 +10415,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
--- web.mjs
+++ web.mjs
@@ -9783,6 +9783,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
